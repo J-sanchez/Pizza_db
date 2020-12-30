43cd27fc600977db23f6b5d77a2c647c4305c584
Jon Sanchez <jonathan.sanchez@windstream.net>
2020-12-30 08:03:25 -0700

    2...

--- models/Pizza.js
+++ models/Pizza.js
@@ -1,22 +1,38 @@
-const { Schema, model } = require('mongoose');
-
-const PizzaSchema = new Schema({ 
-    pizzaName: {
+const PizzaSchema = new Schema(
+    {
+      pizzaName: {
         type: String
-    },
-    createdBy: {
+      },
+      createdBy: {
         type: String
-    },
-    createdAt: {
-        type: Date, 
-        default: Date.now
-    },
-    size: {
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now,
+      },
+      size: {
         type: String,
         default: 'Large'
+      },
+      toppings: [],
+      comments: [
+        {
+          type: Schema.Types.ObjectId,
+          ref: 'Comment'
+        }
+      ]
     },
-    toppings: []
-});
+    {
+      toJSON: {
+        virtuals: true,
+      },
+      id: false
+    }
+  );
+
+PizzaSchema.virtual('commentCount').get(function() {
+    return this.comments.length;
+  });
 
 //Create The Pizza Model
 const Pizza = model('Pizza' , PizzaSchema);
